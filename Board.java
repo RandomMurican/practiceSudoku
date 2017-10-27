fbd80d1609732f5790d73c2bde21e3c3361c06bb
Random Murican <RandomMurican@gmail.com>
2017-10-27 18:03:35 -0500

    Made some progress on value checking

--- Board.java
+++ Board.java
@@ -2,30 +2,49 @@ package com.randommurican.practiceSudoku;
 
 public class Board {
 	private int[][] board;
-	
+	private static int BOARD_SIZE = 9;
+	//i = ROW
+	//j = COLUMN
 	Board() {
-		board = new int[9][9];
-		for(int i = 0; i < 9; i++)
-			for(int j = 0; j < 9; j++)
-				if(setValue(i, j)) {}
+		board = new int[BOARD_SIZE][BOARD_SIZE];
+		for(int ROW = 0; ROW < BOARD_SIZE; ROW++)
+			for(int COLUMN = 0; COLUMN < BOARD_SIZE; COLUMN++)
+				if(setValue(ROW, COLUMN)) {}
 				else {
-					System.out.println("There was an error on panel " + i + ", " + j);
-					i = 9;
-					j = 9;
+					System.out.println("There was an error on panel " + ROW + ", " + COLUMN);
+					ROW = BOARD_SIZE;
+					COLUMN = BOARD_SIZE;
 				}
 	}
 	
-	private boolean setValue(int i, int j) {
-		
-		return false;
+	private boolean setValue(int ROW, int COLUMN) {
+		boolean test = false;
+		for(int ROW_TEST = 1; ROW_TEST <= BOARD_SIZE; ROW_TEST++) {
+			
+		}
+		return test;
 	}
 	
-	private int getGrid(int i, int j) {
-		return (int) (3 * Math.ceil(i/3) + Math.ceil(j/3));
+	private boolean checkGrid(int ROW, int COLUMN, int VALUE) {
+		boolean test = true;
+		for(int BOARD_ROW = 0; BOARD_ROW < BOARD_SIZE; BOARD_ROW++)
+			for(int BOARD_COLUMN = 0; BOARD_COLUMN < BOARD_SIZE; BOARD_COLUMN++)
+				if((int) (3 * Math.ceil(ROW/3) + Math.ceil(COLUMN/3)) == (int) (3 * Math.ceil(BOARD_ROW/3) + Math.ceil(BOARD_COLUMN/3)))
+					if(board[BOARD_ROW][BOARD_COLUMN] == VALUE)
+						return false;
+		return test;
+	}
+	
+	private boolean checkRow(int ROW, int COLUMN, int VALUE) {
+		boolean test = true;
+		for(int BOARD_COLUMN = 0; BOARD_COLUMN < BOARD_SIZE; BOARD_COLUMN++) {
+			// I left off here
+		}
+		return test;
 	}
 }
 
-/* 
+/*    Just for reference
 00 01 02 | 03 04 05 | 06 07 08
 10 11 12 | 13 14 15 | 16 17 18
 20 21 22 | 23 24 25 | 26 27 28
--- Driver.java
+++ Driver.java
@@ -2,9 +2,11 @@ package com.randommurican.practiceSudoku;
 
 public class Driver {
 	public static void main(String[] args) {
-		int MOD_LIMIT = 9;
-		for(int i = 0; i < MOD_LIMIT; i++) {
-			for(int j = 0; j < MOD_LIMIT; j++)
+		
+		// Just playing around with a concept here
+		int BOARD_SIZE = 9;
+		for(int i = 0; i < BOARD_SIZE; i++) {
+			for(int j = 0; j < BOARD_SIZE; j++)
 				System.out.print((int) (3 * Math.ceil(i/3) + Math.ceil(j/3)) + " ");
 			System.out.println("");
 		}
